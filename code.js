dfeb67773cb9487eb864b87af3f7d23543edfd58
Alex <Alex3388@live.ca>
2024-01-29 04:47:26 -0800

    Added 'smooth' scrolling to the js

--- code.js
+++ code.js
@@ -2,7 +2,28 @@ const imageContainer = document.querySelector('.img-container');
 
 imageContainer.addEventListener('wheel', (evt) => {
     evt.preventDefault();
-    imageContainer.scrollBy({
-        left: evt.deltaY < 0 ? -30 : 30,
-    });
+    smoothScroll(imageContainer, evt.deltaY < 0 ? -30 : 30);
 });
+
+function smoothScroll(element, scrollAmount) {
+    let start = null;
+    const step = timestamp => {
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        const fraction = progress / 10000; // Adjust as needed
+
+        if (fraction < 1) {
+            element.scrollBy({
+                left: scrollAmount * fraction,
+                behavior: 'auto'
+            });
+            requestAnimationFrame(step);
+        } else {
+            element.scrollBy({
+                left: scrollAmount,
+                behavior: 'auto'
+            });
+        }
+    };
+    requestAnimationFrame(step);
+}
